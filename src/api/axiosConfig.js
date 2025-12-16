3015fc380b7c86afacddf9e5b26720e288b7bca9
Thomas <tkurian2@gmail.com>
2025-12-16 14:56:47 +0530

    deploy vars

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: `${process.env.REACT_APP_BASE_URL}/api`, // Matches your Backend Port
+    baseURL: `${process.env.REACT_APP_BASE_URL}/api`, 
     headers: {
         'Content-Type': 'application/json',
     },
